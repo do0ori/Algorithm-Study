54431608087f33a86bd4dd02f48d84ac52497994
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-04-01 01:54:27 +0900

    [level 2] Title: 가장 큰 수, Time: 59.58 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -1,17 +1,21 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
 string solution(vector<int> numbers) {
-    sort(numbers.begin(), numbers.end(), [](int a, int b) {
-        string c = to_string(a), d = to_string(b);
-        return c + d > d + c;
-    });
-    
-    string answer = "";
-    for (auto n : numbers) {
-        answer += to_string(n);
+    vector<string> strs;
+    for (int x : numbers) {
+        strs.push_back(to_string(x));
     }
+
+    sort(strs.begin(), strs.end(), [](const string& a, const string& b) {
+        return a + b > b + a;
+    });
+
+    string answer = "";
+    for (const string& s : strs) {
+        answer += s;
+    }
+
     // 모든 숫자가 0인 경우 0 하나만 반환해야 함
     return answer[0] == '0' ? "0" : answer;
 }
