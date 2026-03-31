3c101644899f557f1e37eec83f0a0bcf7c3d103b
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-04-01 02:00:30 +0900

    [level 2] Title: H-Index, Time: 0.04 ms, Memory: 4.19 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> citations) {
+    sort(citations.begin(), citations.end());
+    for (int i = 0; i < citations.size(); i++) {
+        if (citations[i] >= citations.size() - i) {
+            return citations.size() - i;
+        }
+    }
+    return 0;
+}
