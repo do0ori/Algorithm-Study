f05d733e90775855672425c2ab891e0dd00983c7
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-04-01 20:41:54 +0900

    [Silver III] Title: 이친수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2193. 이친수/이친수.py
+++ 백준/Silver/2193. 이친수/이친수.py
@@ -1,9 +1,10 @@
-# 나열하면 피보나치됨
+# 0으로 끝나는 개수, 1로 끝나는 개수
 N = int(input())
-dp = [0] * (N + 1)
-dp[1] = 1
+dp = [[0] * 2 for _ in range(N + 1)]
+dp[1] = [0, 1]
 
 for i in range(2, N + 1):
-    dp[i] = dp[i - 2] + dp[i - 1]
+    dp[i][0] = sum(dp[i - 1])
+    dp[i][1] = dp[i - 1][0]
 
-print(dp[N])
+print(dp[N][0] + dp[N][1])
