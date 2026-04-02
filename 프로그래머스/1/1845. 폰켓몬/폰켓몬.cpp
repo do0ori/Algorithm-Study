bfab40b46465beb1217c6b83af2a405384908a6b
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-04-02 20:31:57 +0900

    [level 1] Title: 폰켓몬, Time: 1.45 ms, Memory: 4.18 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(vector<int> nums)
+{
+    set<int> s;
+    for (int num : nums) {
+        s.insert(num);
+    }
+    
+    return min(s.size(), nums.size() / 2);
+}
