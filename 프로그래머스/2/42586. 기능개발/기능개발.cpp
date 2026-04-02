2585e438bba004f8ff003828fce372deed2600f7
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-04-03 01:39:18 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.cpp
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
+/*
+// queue 버전
 vector<int> solution(vector<int> progresses, vector<int> speeds) {
     vector<int> answer;
     queue<int> days_left;
@@ -18,6 +19,31 @@ vector<int> solution(vector<int> progresses, vector<int> speeds) {
         }
         answer.push_back(cnt);
     }
+    
+    return answer;
+}
+*/
+
+// queue 없는 버전
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> days_left;
+    for (int i = 0; i < progresses.size(); i++) {
+        days_left.push_back(ceil((double)(100 - progresses[i]) / speeds[i]));
+    }
+    
+    vector<int> answer;
+    int cnt = 1;
+    int deploy = days_left[0];
+    for (int i = 1; i < days_left.size(); i++) {
+        if (days_left[i] <= deploy) {
+            cnt++;
+        } else {
+            deploy = days_left[i];
+            answer.push_back(cnt);
+            cnt = 1;
+        }
+    }
+    answer.push_back(cnt);
     
     return answer;
 }
