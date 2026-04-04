3e6446eb5d7e52c3b3bfe9730d126914f5512bd8
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-04-05 00:25:55 +0900

    [Silver II] Title: 부분수열의 합, Time: 228 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -6,24 +6,19 @@ N, S = map(int, input().split())
 A = list(map(int, input().split()))
 
 answer = 0
-visited = [False] * N
 
 
-def dfs(l=0, s=0):
+def dfs(l=0, s=0, cnt=0):
     global answer
 
     if l == N:
-        if s == S:
+        if s == S and cnt > 0:
             answer += 1
         return
 
-    dfs(l + 1, s)
-
-    visited[l] = True
-    dfs(l + 1, s + A[l])
-    visited[l] = False
+    dfs(l + 1, s, cnt)  # 선택 O
+    dfs(l + 1, s + A[l], cnt + 1)  # 선택 X
 
 
 dfs()
-# S가 0이면 아무것도 고르지 않은 경우에도 충족되므로 1 빼주기
-print(answer if S != 0 else answer - 1)
+print(answer)
