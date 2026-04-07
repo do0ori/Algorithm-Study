2d728e53cdf2b412771558cb6d9c1780d3240818
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-04-07 22:07:17 +0900

    [level 3] Title: 네트워크, Time: 0.05 ms, Memory: 4.14 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -4,7 +4,7 @@
 
 using namespace std;
 
-void bfs(int c, int n, vector<vector<int>>& computers, int* visited) {
+void bfs(int c, vector<vector<int>>& computers, int* visited) {
     queue<int> q;
     q.push(c);
     
@@ -12,11 +12,35 @@ void bfs(int c, int n, vector<vector<int>>& computers, int* visited) {
         int computer = q.front();
         q.pop();
         
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < computers.size(); i++) {
             if (visited[i] == 0 && computers[computer][i] == 1) {
                 visited[i] = 1;
                 q.push(i);
             }
+        }
+    }
+}
+
+int solution_bfs(int n, vector<vector<int>> computers) {
+    int cnt = 0;
+    int visited[200] = {0,};
+    for (int i = 0; i < n; i++) {
+        if (visited[i] == 0) {
+            visited[i] = 1;
+            bfs(i, computers, visited);
+            cnt++;
+        }
+    }
+    
+    return cnt;
+}
+
+void dfs(int c, vector<vector<int>>& computers, int* visited) {
+    visited[c] = 1;
+
+    for (int i = 0; i < computers.size(); i++) {
+        if (visited[i] == 0 && computers[c][i] == 1) {
+            dfs(i, computers, visited);
         }
     }
 }
@@ -26,8 +50,7 @@ int solution(int n, vector<vector<int>> computers) {
     int visited[200] = {0,};
     for (int i = 0; i < n; i++) {
         if (visited[i] == 0) {
-            visited[i] = 1;
-            bfs(i, n, computers, visited);
+            dfs(i, computers, visited);
             cnt++;
         }
     }
