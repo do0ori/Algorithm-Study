89cf274b6051d2036aa91a349d13ad8d743c248b
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-04-10 18:23:49 +0900

    [level 3] Title: 이중우선순위큐, Time: 9.28 ms, Memory: 11 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.cpp
@@ -1,56 +1,26 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <unordered_map>
-#include <iostream>
+#include <set>
 
 using namespace std;
 
-priority_queue<int> max_q;
-priority_queue<int, vector<int>, greater<int>> min_q;
-unordered_map<int, int> manage;
-
-void sync_max() {
-    while (!max_q.empty() && manage[max_q.top()] == 0) {
-        max_q.pop();
-    }
-}
-
-void sync_min() {
-    while (!min_q.empty() && manage[min_q.top()] == 0) {
-        min_q.pop();
-    }
-}
-
 vector<int> solution(vector<string> operations) {
-    for (string operation : operations) {
-        char command = operation[0];
-        int number = stoi(operation.substr(2));
-        
-        if (command == 'I') {
-            max_q.push(number);
-            min_q.push(number);
-            manage[number]++;
-        } else {    // 'D'
-            if (number == 1) {
-                sync_max();
-                if (!max_q.empty()) {
-                    manage[max_q.top()]--; max_q.pop();                    
-                }
-            } else {    // -1
-                sync_min();
-                if (!min_q.empty()) {
-                    manage[min_q.top()]--; min_q.pop();
-                }
+    multiset<int> ms;
+
+    for (const string& op : operations) {
+        if (op[0] == 'I') {
+            ms.insert(stoi(op.substr(2)));
+        } else {
+            if (ms.empty()) continue;
+
+            if (op[2] == '1') {
+                ms.erase(prev(ms.end()));
+            } else {
+                ms.erase(ms.begin());
             }
         }
     }
-    
-    sync_max(); sync_min();
-    
-    if (!max_q.empty() && !min_q.empty()) {
-        return {max_q.top(), min_q.top()};
-    } else {
-        return {0, 0};
-    }
+
+    if (ms.empty()) return {0, 0};
+    return {*prev(ms.end()), *ms.begin()};
 }
