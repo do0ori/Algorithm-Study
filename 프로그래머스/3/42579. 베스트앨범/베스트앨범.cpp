ae66a07af04acb2b86a4eddf1a57572d834160ab
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-04-11 00:03:45 +0900

    [level 3] Title: 베스트앨범, Time: 0.04 ms, Memory: 4.13 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 vector<int> solution(vector<string> genres, vector<int> plays) {
-    unordered_map<string, priority_queue<vector<int>>> group_by_genre;
+    unordered_map<string, priority_queue<pair<int, int>>> group_by_genre;
     unordered_map<string, int> genre_cnt;
     for (int i = 0; i < genres.size(); i++) {
         genre_cnt[genres[i]] += plays[i];
@@ -11,16 +11,17 @@ vector<int> solution(vector<string> genres, vector<int> plays) {
     }
     
     vector<pair<string, int>> genre_cnt_sorted(genre_cnt.begin(), genre_cnt.end());
-    sort(genre_cnt_sorted.begin(), genre_cnt_sorted.end(), [](const auto& a, const auto& b){
-        return a.second > b.second;
-    });
+    sort(genre_cnt_sorted.begin(), genre_cnt_sorted.end(),
+         [](const auto& a, const auto& b){
+             return a.second > b.second;
+         });
     
     vector<int> answer;
     for (auto [g, v] : genre_cnt_sorted) {
-        auto songs = group_by_genre[g];
+        auto& songs = group_by_genre[g];
         for (int i = 0; i < 2; i++) {
             if (songs.empty()) break;
-            answer.push_back(-songs.top()[1]);
+            answer.push_back(-songs.top().second);
             songs.pop();
         }
     }
