b30107709b9a522c032242eeccff01bb2d91f083
JaeKyung Hwang <71831926+do0ori@users.noreply.github.com>
2026-05-01 01:52:33 +0900

    Add: generate-parentheses - submission-1

---
+++ Data Structures & Algorithms/generate-parentheses/submission-1.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> answer;
+
+    void bt(int n, string group, int open, int close) {
+        if (open == n && close == n) {
+            answer.push_back(group);
+            return;
+        }
+
+        if (open < n) {
+            bt(n, group + "(", open + 1, close);
+        }
+
+        if (open > close) {
+            bt(n, group + ")", open, close + 1);
+        }
+    }
+
+    vector<string> generateParenthesis(int n) {
+        bt(n, "", 0, 0);
+        return answer;
+    }
+};
